075797c89074656b48687bf7600dd77014e795f2
santosh sagar <msg2santoshsagar@gmail.com>
2017-05-05 23:58:23 +0530

    Initial Commit

---
+++ src/main/java/com/sample/Application.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Application {
+	
+	 public static void main(String[] args) {
+	        SpringApplication app = new SpringApplication(Application.class);
+	        app.run(args);
+	 }
+	   
+
+}
